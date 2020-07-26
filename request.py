deb6bcd49a3492989fbcde9eb1928181216ccb07
JahirGithub <60692814+JahirGithub@users.noreply.github.com>
2020-07-26 12:09:08 +0530

    Add files via upload

---
+++ request.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 26 11:54:57 2020
+
+@author: Jahiruddin
+"""
+
+
+import requests
+
+url = 'http://localhost:5000/predict_api'
+r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+
+print(r.json())
